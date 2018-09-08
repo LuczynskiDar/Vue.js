7235b6798c79089724917ff0b6fa40f7db593f97
darek <rayzki@gmail.com>
2018-09-08 13:12:53 +0200

    [#2.2]

--- cli8_project/src/App.vue
+++ cli8_project/src/App.vue
@@ -1,27 +1,55 @@
+<!--10- 1. There's a <template> part -->
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>My awesome list</h2>
+    <ul>
+      <li v-for="p in products" :key="p.id">{{ p.name }}</li>
+    </ul>
+    <p v-if="!products.length">No products!</p>
+    <button v-on:click="removeLast()">Remove last item</button>
+    <button v-on:click="addItem()">Add item</button>
   </div>
 </template>
 
+<!--21- 2. A <script> part -->
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// 4. Now App is not mounted itself, we're just creating a component (more on that later - hold your horses!)
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  //11/ 5. Data can no longer be just an object to prevent accidental shared state
+  data() {
+    return {
+      products: [{
+        id: 0,
+        name: 'Coffee'
+      }, {
+        id: 1,
+        name: 'Pizza'
+      }]
+    }
+  },
+  methods: {
+    removeLast() {
+      this.products.pop();
+    },
+    addItem(){
+    this.products.push({
+        id: 4,
+        name: 'Susage'
+      })
+
+    }
+    
   }
 }
 </script>
 
+<!--9- 3. And <style> part -->
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
